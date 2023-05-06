4234241597803e4076584b1c73d630efab754906
Patryk Mielech <patryk.mielech@protonmail.com>
2023-05-06 22:07:36 +0200

    Serial write & Serial status
    Added user input message, clear received message textbox. Added info
    about connection status.

--- canApp/canApp/ViewModels/MainWindowViewModel.cs
+++ canApp/canApp/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Threading;
@@ -17,15 +18,18 @@ public class MainWindowViewModel : ViewModelBase
 {
     public ICommand RefreshComPortsCommand { get; }
     public ICommand ConnectToComCommand { get; }
+    public ICommand SendUserMessage { get; }
+
+    public ICommand ClearReceivedData { get; }
+
     public ComViewModel ComList { get; }
 
-    private SerialPort serial = new SerialPort();
+    private readonly SerialPort _serial = new SerialPort();
+    
 
     public MainWindowViewModel(DebugComList cp)
     {
-
         ComList = new ComViewModel(cp.GetItems());
-        Selected_com = 0;
 
         RefreshComPortsCommand = ReactiveCommand.Create(() =>
         {
@@ -40,53 +44,123 @@ public class MainWindowViewModel : ViewModelBase
                 throw;
             }
 
-            for (int i = 0; i < serialPortList.Length; i++)
+            foreach (var port in serialPortList)
             {
-                ComList.Items.Add(new ComPort() { Name = serialPortList[i] });
+                ComList.Items.Add(new ComPort() { Name = port });
             }
+        });
+        
+        SendUserMessage = ReactiveCommand.Create(() =>
+        {
+            if (IsConnected)
+            {
+                try
+                {
+                    // Send the binary data out the port
+                    byte[] hexstring = Encoding.ASCII.GetBytes(UserInput);
+                    //There is a intermitant problem that I came across
+                    //If I write more than one byte in succesion without a 
+                    //delay the PIC i'm communicating with will Crash
+                    //I expect this id due to PC timing issues ad they are
+                    //not directley connected to the COM port the solution
+                    //Is a ver small 1 millisecound delay between chracters
+                    foreach (byte hexValue in hexstring)
+                    {
+                        byte[] _hexValue = new byte[] { hexValue }; // need to convert byte to byte[] to write
+                        _serial.Write(_hexValue, 0, 1);
+                        Thread.Sleep(1);
+                    }
+                    
+                    var date = DateTime.Now.ToString("HH:mm:ss");
+                    ReceivedData += $"==> TX<{date}> ~ {UserInput}\n";
+                    UserInput = "";
+
+                }
+                catch (Exception ex)
+                {
+                    ReceivedData += "~~~Failed to send data!~~~\n";
+
+                }
+                
+                
+            }
+            
+            
+        });     
+            
+        ClearReceivedData = ReactiveCommand.Create(() =>
+        {
+            ReceivedData = "";
         });
 
         ConnectToComCommand = ReactiveCommand.Create(() =>
         {
-
-            serial.PortName = ComList.Items.ElementAt(Selected_com).Name;
-            serial.BaudRate = 9600;
-            serial.Handshake = System.IO.Ports.Handshake.None;
-            serial.Parity = Parity.None;
-            serial.DataBits = 8;
-            serial.StopBits = StopBits.Two;
-            serial.ReadTimeout = 200;
-            serial.WriteTimeout = 50;
-            try
+            if (ConnectionButton == "Connect")
             {
-                serial.Open();
-                serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(Recieve);
+                //TODO Exception when choosing wrong com port (unix) 
+                _serial.PortName = ComList.Items.ElementAt(SelectedCom).Name;
+                _serial.BaudRate = 9600;
+                _serial.Handshake = System.IO.Ports.Handshake.None;
+                _serial.Parity = Parity.None;
+                _serial.DataBits = 8;
+                _serial.StopBits = StopBits.Two;
+                _serial.ReadTimeout = 200;
+                _serial.WriteTimeout = 50;
+                try
+                {
+                    _serial.Open();
+                    IsConnected = _serial.IsOpen;
+                    if (IsConnected == true)
+                    {
+                        StatusColor = "Green";
+                    }
+                    ReceivedData += "~~~Connected Successful!~~~\n";
+                    Thread.Sleep(10);
+                    _serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(Receive);
+                    ConnectionButton = "Disconnect";
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
 
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e);
-                throw;
-            }
+                try
+                {
+                    _serial.Close();
+                    IsConnected = _serial.IsOpen;
+                    if (IsConnected == false)
+                    {
+                        StatusColor = "Red";
+                    }
 
+                    ConnectionButton = "Connect";
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                
+            }
+            
         });
 
     }
 
-    private void Recieve(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+    private void Receive(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
     {
         // Collecting the characters received to our 'buffer' (string).
-        //Recived_data = "";
-        //Recived_data = serial.ReadExisting(); 
-        var serialDevice = sender as SerialPort;
-        var buffer = new byte[serial.BytesToRead];
-        serialDevice.Read(bytes, 0, buffer.Length);
+        var date = DateTime.Now.ToString("HH:mm:ss");
+        ReceivedData += $"<== Rx<{date}> ~ {_serial.ReadExisting()}";
+        
 
-        // process data on the GUI thread
-        Application.Current.Dispatcher.Invoke(new Action(() => {
-            
-            
-        }));
     }
     
 
--- canApp/canApp/ViewModels/ViewModelBase.cs
+++ canApp/canApp/ViewModels/ViewModelBase.cs
@@ -10,18 +10,51 @@ namespace canApp.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
-    private int _selected_com;
-    public int Selected_com
+    
+    private string _statusColor = "Red";
+
+    protected string StatusColor
     {
-        get => _selected_com;
-        set => this.RaiseAndSetIfChanged(ref _selected_com, value);
+        get => _statusColor;
+        set => this.RaiseAndSetIfChanged(ref _statusColor, value);
     }
     
-    private string _recived_data;
-    public string Recived_data
+    
+    private bool _isConnected = false;
+
+    protected bool IsConnected
     {
-        get => _recived_data;
-        set => this.RaiseAndSetIfChanged(ref _recived_data, value);
+        get => _isConnected;
+        set => this.RaiseAndSetIfChanged(ref _isConnected, value);
+    }
+    
+    private string _connectionButton = "Connect";
+
+    protected string ConnectionButton
+    {
+        get => _connectionButton;
+        set => this.RaiseAndSetIfChanged(ref _connectionButton, value);
+    }
+    
+    private string _userInput = "";
+    public string UserInput
+    {
+        get => _userInput;
+        set => this.RaiseAndSetIfChanged(ref _userInput, value);
+    }
+    
+    private int _selectedCom = 0;
+    public int SelectedCom
+    {
+        get => _selectedCom;
+        set => this.RaiseAndSetIfChanged(ref _selectedCom, value);
+    }
+    
+    private string _receivedData = "";
+    public string ReceivedData
+    {
+        get => _receivedData;
+        set => this.RaiseAndSetIfChanged(ref _receivedData, value);
     }
 }
     
